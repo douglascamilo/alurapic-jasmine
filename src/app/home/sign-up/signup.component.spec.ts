62ca70aed12f232a9ffde891925e09da8f89769b
Douglas Sousa <douglascamilo@gmail.com>
2020-06-27 18:52:58 -0300

    SignUpComponent - Criacao cenario para cadastrar usuario

--- src/app/home/sign-up/signup.component.spec.ts
+++ src/app/home/sign-up/signup.component.spec.ts
@@ -1,13 +1,17 @@
 import { SignUpComponent } from './signup.component';
 import { async, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
 import { VMessageModule } from '../../shared/componets/vmessage/vmessage.module';
 import { RouterTestingModule } from '@angular/router/testing';
 import { SignUpService } from './signup.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
-describe(`Testes unitario para: ${SignUpComponent.name}`, () => {
+describe('Componente: SignUpComponent', () => {
   let componente: SignUpComponent;
+  let router: Router;
+  let signUpService: SignUpService;
 
   beforeEach(async(() => {
     TestBed
@@ -25,6 +29,9 @@ describe(`Testes unitario para: ${SignUpComponent.name}`, () => {
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    signUpService = TestBed.get(SignUpService);
+
     const fixture = TestBed.createComponent(SignUpComponent);
     componente = fixture.componentInstance;
     fixture.detectChanges();
@@ -32,5 +39,19 @@ describe(`Testes unitario para: ${SignUpComponent.name}`, () => {
 
   it('deve ser instanciado', () => {
     expect(componente).toBeDefined();
+  });
+
+  it('deve cadastrar um usuario', () => {
+    spyOn(router, 'navigate');
+    spyOn(signUpService, 'signup').and.returnValue(of({}));
+
+    const formulario = componente.signupForm;
+    formulario.get('email').setValue('douglas@douglas.com');
+    formulario.get('fullName').setValue('Douglas Sousa');
+    formulario.get('userName').setValue('douglas');
+    formulario.get('password').setValue('123');
+    componente.signUp();
+
+    expect(router.navigate).toHaveBeenCalledWith(['']);
   });
 });
